552434fb31753d7648a262fe9a6b4496c7b035a8
Aleexdeleon2508@outlook.com <Aleexdeleon2508@outlook.com>
2021-09-10 23:39:54 -0600

    Commit inicial

---
+++ WebRepuestosOrellana/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRepuestosOrellana.Models
+{
+    public class Empleado
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string DPI { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string Celular { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public Decimal ComisionPorVentas { get; set; }
+    }
+}
---
+++ WebRepuestosOrellana/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebRepuestosOrellana.Startup))]
+namespace WebRepuestosOrellana
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
